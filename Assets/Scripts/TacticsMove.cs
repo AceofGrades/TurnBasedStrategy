d1210711ed9fc88788c60699b999cbc095079bae
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2020-06-30 14:20:49 +1000

    Added Tactics Move script, added tiles, etc.

---
+++ Assets/Scripts/TacticsMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsMove : MonoBehaviour
+{
+    List<Tile> selectableTiles = new List<Tile>();
+
+    public int move = 5;
+    public float jumpHeight = 2;
+
+    Vector3 velocity = new Vector3();
+    Vector3 heading = new Vector3();
+
+    protected void Init()
+    {
+
+    } 
+}
