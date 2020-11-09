f3b974dbe66161f0887e94f602dc761e827fda29
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2020-11-09 13:56:45 +1100

    Getting back on track

--- Assets/Scripts/Model/LevelData.cs
+++ Assets/Scripts/Model/LevelData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class LevelData : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
