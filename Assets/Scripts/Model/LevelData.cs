175255bbb6772fddb47e49a291e9e8c033209799
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2020-11-09 14:30:53 +1100

    BoardCreator made

--- Assets/Scripts/Model/LevelData.cs
+++ Assets/Scripts/Model/LevelData.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelData : MonoBehaviour
+public class LevelData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<Vector3> tiles;
 }
