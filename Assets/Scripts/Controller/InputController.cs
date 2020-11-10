ddd2e62f77b6f68df314f82814a02646a776efd5
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2020-11-10 12:41:05 +1100

    Added InputController, and EventArgs

---
+++ Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    Repeater _hor = new Repeater("Horizontal");
+    Repeater _ver = new Repeater("Vertical");
+    string[] _buttons = new string[] { "Fire1", "Fire2", "Fire3" };
+
+    public static event EventHandler<InfoEventArgs<Point>> moveEvent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+        int x = _hor.Update();
+        int y = _ver.Update();
+        if (x != 0 || y != 0)
+        {
+            if (moveEvent != null)
+                moveEvent(this, new InfoEventArgs<Point>(new Point(x, y)));
+        }
+
+        for (int i = 0; i < 3; ++i)
+        {
+            if (Input.GetButtonUp(_buttons[i]))
+            {
+                if(fireEvent != null)
+                {
+                    fireEvent(this, new InfoEventArgs<int>(i));
+                }
+            }
+        }
+    }
+
+    public static event EventHandler<InfoEventArgs<int>> fireEvent;
+}
