265a8f086d6929cfaaa08ef0112439e7046e1e64
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2020-11-19 12:12:17 +1100

    Added conversations. Fixed the movement, and heroes can now spawn

---
+++ Assets/Editor/AssetCreator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetCreator
+{
+    [MenuItem("Assets/Create/Conversation Data")]
+    public static void CreateConversationData()
+    {
+        ScriptableObjectUtility.CreateAsset<ConversationData>();
+    }
+}
