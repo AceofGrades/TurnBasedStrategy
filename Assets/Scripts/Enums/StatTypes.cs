b00f29fec928df1d83ab2939877be9dbace243be
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2020-11-23 11:49:50 +1100

    Added Stats and Experience Manager

---
+++ Assets/Scripts/Enums/StatTypes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StatTypes
+{
+    LVL, // Level
+    EXP, // Experience
+    HP,  // Hit Points
+    MHP, // Max Hit Points
+    AP,  // Ability Points
+    MAP, // Max Ability Points
+    ATK, // Physical Attack
+    DEF, // Physical Defense
+    AAT, // Ability Attack
+    ADF, // Ability Defense
+    EVD, // Evade
+    RES, // Status Resistance
+    SPD, // Speed
+    MOV, // Move Range
+    JMP, // Jump Height
+    Count
+}
