f8dbf2f7072927a2a86048a94d483c6226055efd
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2020-11-24 12:39:36 +1100

    Added Jobs to the game

---
+++ Assets/Scripts/Enums/EquipSlots.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Flags]
+public enum EquipSlots
+{
+    None = 0,
+  Primary = 1 << 0,   // usually a weapon (gun, etc)
+  Secondary = 1 << 1,  // usually a pistol, but could be another gun
+  Head = 1 << 2,    // helmet, hat, etc
+  Body = 1 << 3,    // body armor
+  Accessory = 1 << 4  // ring, belt, etc
+}
