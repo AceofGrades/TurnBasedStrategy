d92d282d86e8a792fa522d9443be7a09cc716a6f
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2020-11-30 15:48:57 +1100

    Fixed Hit Rate! It now works as intended

--- Assets/Scripts/Extensions/FacingsExtensions.cs
+++ Assets/Scripts/Extensions/FacingsExtensions.cs
@@ -9,9 +9,9 @@ public static class FacingsExtensions
         Vector2 targetDirection = target.dir.GetNormal();
         Vector2 approachDirection = ((Vector2)(target.tile.pos - attacker.tile.pos)).normalized;
         float dot = Vector2.Dot(approachDirection, targetDirection);
-        if (dot >= 0.45f)
+        if (dot >= 0.72f)
             return Facings.Back;
-        if (dot <= -0.45f)
+        if (dot <= -0.7f)
             return Facings.Front;
         return Facings.Side;
     }
